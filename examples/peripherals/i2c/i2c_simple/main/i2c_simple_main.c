b0d8791587f040e876d24fd68238b4ced6dd44af
Lichuan Li <lapchuen.lee@gmail.com>
2023-12-08 19:54:17 +1100

    test_main

--- examples/peripherals/i2c/i2c_simple/main/i2c_simple_main.c
+++ examples/peripherals/i2c/i2c_simple/main/i2c_simple_main.c
@@ -24,7 +24,8 @@ static const char *TAG = "i2c-simple-example";
 
 #define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
 #define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_PORT_NUM_0              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define TEST_I2C_PORT               1
 #define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
@@ -36,7 +37,21 @@ static const char *TAG = "i2c-simple-example";
 #define MPU9250_PWR_MGMT_1_REG_ADDR         0x6B        /*!< Register addresses of the power managment register */
 #define MPU9250_RESET_BIT                   7
 
-void app_main(void)
-{
+void app_main(void) {
 
+    // 1. Master Bus Configuration
+    i2c_master_bus_config_t i2c_mst_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT, 
+        .i2c_port = TEST_I2C_PORT,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = false,
+    };
+
+    i2c_master_bus_handle_t bus_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+    ESP_ERROR_CHECK(i2c_master_probe(bus_handle, 0x33, -1));
+    ESP_ERROR_CHECK(i2c_del_master_bus(bus_handle));
 }
